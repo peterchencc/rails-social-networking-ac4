e8e7306c9ab9cf5dc4f88ce409b728c23cd5dffb
peterchencc <peterchencc@gmail.com>
2015-04-21 15:55:31 +0800

    add devise omniauthable for Facebook

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
 
+gem 'omniauth-facebook'
 
 
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,6 +234,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  fb = YAML.load(File.read("#{Rails.root}/config/facebook.yml"))[Rails.env]
+  config.omniauth :facebook, fb["app_id"], fb["secret"]
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
